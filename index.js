1fb37adc59f212d7665ba83fde4e3b58904c6862
Gnimmos <gnicolaou92@gmail.com>
2021-05-28 11:47:34 +0300

    updated cross origin

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.static('public'));
 app.use(  cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3000'+'http://wolvestalk.commedia.wiki',
     credentials: false,
   }));
   
