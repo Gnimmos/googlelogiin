4474be674329bbe2013165e0f22845cd9a4af226
Gnimmos <gnicolaou92@gmail.com>
2021-05-28 12:35:23 +0300

    modified:   index.js

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static('public'));
+
 app.use(  cors({
     origin: 'http://wolvestalk.commedia.wiki',
     credentials: false,
@@ -36,8 +37,9 @@ app.use(  cors({
   }));
   
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://wolvestalk.commedia.wiki/"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Origin: *');
+    res.header('Access-Control-Allow-Methods: *');
+    res.header('Access-Control-Allow-Headers: *');
     next();
   });
 
